44f755d4a87aac58aed5abc2caa57e1f27419a1a
Ravi <ravi.cheetirala@tlconsulting.com.au>
2025-05-12 14:05:33 +1000

    changes to AMI

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 AMI for us-east-1
+  ami           = "ami-ff"  # Amazon Linux 2 AMI for us-east-1
   instance_type = "t2.micro"
 
   tags = {
